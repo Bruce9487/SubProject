3757d7eeede41e48a8a428529d4975b30a40f9c6
APP技術部-陳冠志 <bruce@mitake.com.tw>
2020-06-18 17:14:47 +0800

    Initial Commit

---
+++ SubProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SubProject
+//
+//  Created by APP技術部-陳冠志 on 2020/6/18.
+//  Copyright © 2020 Bruce Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
