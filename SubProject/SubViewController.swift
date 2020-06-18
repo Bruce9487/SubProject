fd0a5f8adb0009558ad014e74a8dbb0fcfab2cb5
APP技術部-陳冠志 <bruce@mitake.com.tw>
2020-06-18 17:50:07 +0800

    test

--- SubProject/SubViewController.swift
+++ SubProject/SubViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SubViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("SubViewController")
     }
 
 
